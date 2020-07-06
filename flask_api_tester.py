1823adc34e8a2a8ec9a9f5b0ca53d8f62a93ee20
yumjw <ski02049@naver.com>
2020-07-06 14:46:30 +0900

    tester_note_add

--- flask_api_tester.py
+++ flask_api_tester.py
@@ -7,5 +7,5 @@ with open('test.json', mode='r', encoding='utf-8-sig') as json_file:
         json_file.seek(0)
     json_data = json.load(json_file)
 
-res = requests.get('http://3.133.76.141:10001/plus?user_musts=["양배추"]&user_options=["햄"]')    # AWS
-# res = requests.post("http://3.133.76.141:5000/recipes/", json=json_data) # AWS Original
+res = requests.get('http://3.133.76.141:10001/plus?user_musts=["양배추"]&user_options=["햄"]')    # GET
+# res = requests.post("http://3.133.76.141:5000/recipes/", json=json_data) # POST
